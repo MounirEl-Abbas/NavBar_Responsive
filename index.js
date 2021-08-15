8359a323f3d3b490973ee488cdaefdd5c529a7cd
MounirEl-Abbas <87664712+MounirEl-Abbas@users.noreply.github.com>
2021-08-15 14:03:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector('.header-container > div > span')
+const menuItems = document.querySelector('.menu-items')
+const listItems = document.querySelector('li')
+
+menuBtn.addEventListener("click", function() {
+  menuItems.classList.toggle("slide-down")
+  
+  })
+
+
+
+
